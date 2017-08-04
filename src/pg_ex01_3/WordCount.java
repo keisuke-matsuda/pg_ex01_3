1bcdab04bd89f0846289d9a48978ce4d73104379
user <user@user-PC>
2017-08-04 11:13:19 +0900

    first commit!

---
+++ src/pg_ex01_3/WordCount.java
@@ -0,0 +1,25 @@
+package pg_ex01_3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WordCount {
+	public static void main(String[] args) {
+		try {
+			File file = new File("src/pg_ex01_3/sample.txt");
+			BufferedReader br = new  BufferedReader(new FileReader(file));
+
+			String line = br.readLine();
+			while (line != null ){
+				System.out.println(line);
+				line = br.readLine();
+			}
+			br.close();
+			}catch (IOException e){
+				System.out.println(e);
+		}
+	}
+
+}
