f901605596b83f64a504a4df92c51451a19c5676
user <user@user-PC>
2017-08-04 11:48:00 +0900

    失敗作。。。

--- src/pg_ex01_3/WordCount.java
+++ src/pg_ex01_3/WordCount.java
@@ -4,26 +4,31 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
+
 
 public class WordCount {
-	public static void main(String[] args) {
+	public  static void main(String[] args){
+		String[] word = null;
+		List<String> wordList = new ArrayList<String>();
+
+		read(wordList)
+	}
+	public static void read(wordList) {
 		try {
 			File file = new File("src/pg_ex01_3/sample.txt");
 			BufferedReader br = new  BufferedReader(new FileReader(file));
 
 			String line = br.readLine();
-			while (line != null ){
+			while (line 	!= null ){
 				System.out.println(line);
 				line = br.readLine();
 			}
 			br.close();
 			}catch (IOException e){
 				System.out.println(e);
-
-			//sample
-			//sample
-			//sample
 		}
 	}
+
 
 }
