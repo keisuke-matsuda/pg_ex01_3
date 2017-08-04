8b19e36a984918eeb64617a2ed3f09d60b7f2477
user <user@user-PC>
2017-08-04 11:49:21 +0900

    ?

--- src/pg_ex01_3/WordCount.java
+++ src/pg_ex01_3/WordCount.java
@@ -4,23 +4,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
-
 
 public class WordCount {
-	public  static void main(String[] args){
-		String[] word = null;
-		List<String> wordList = new ArrayList<String>();
-
-		read(wordList)
-	}
-	public static void read(wordList) {
+	public static void main(String[] args) {
 		try {
 			File file = new File("src/pg_ex01_3/sample.txt");
 			BufferedReader br = new  BufferedReader(new FileReader(file));
 
 			String line = br.readLine();
-			while (line 	!= null ){
+			while (line != null ){
 				System.out.println(line);
 				line = br.readLine();
 			}
@@ -30,5 +22,7 @@ public class WordCount {
 		}
 	}
 
-
 }
+
+//sample sample sample
+//sample sample sample
